adec9bab896beec28d41b9b22e5669c488ce3b6e
Kiril Kirilov <kirilov.mnt@gmail.com>
2016-07-30 16:52:13 +0100

    API queries moved to services. Bug when updating accordion list

--- www/js/services.js
+++ www/js/services.js
@@ -135,3 +135,74 @@ angular.module('app.services', [])
         }
     }
 }])
+
+.service('infoWindow', [function () {
+    this.showFor = function (map, busStop, status) {
+        if (status) {
+            busStop.infowindow.open(map, busStop.marker)
+        } else {
+            busStop.infowindow.close()
+        }
+    }
+}])
+
+
+.service('queryApi', ['$state', function ($state) {
+    queryApi = this
+    var app_id = queryApi.app_id = "928e6aca"
+    var app_key = queryApi.app_key = "aa747294d32b6f68b6a827ed7f79242f"
+
+    queryApi.nearStops = function (coords, range, map) {
+        var baseUrl = "https://transportapi.com/v3/uk/bus/stops/near.json?"
+        var url = baseUrl + "app_id=" + app_id + "&app_key=" + app_key + "&lat=" + coords.lat + "&lon=" + coords.lng + "&page=1" + "&rpp=" + range + "&callback=?"
+
+        if (typeof (queryApi.nearStops.stopsList) != "undefined") {
+            var list = queryApi.nearStops.stopsList
+            for (i = 0; i < list.length; i++) {
+                list[i].marker.setMap(null)
+            }
+        }
+        $.getJSON(url, function (data) {
+            console.log(url)
+            for (var i = 0; i < data.stops.length; i++) {
+                var busStop = data.stops[i]
+                busStop.marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(busStop.latitude, busStop.longitude),
+                    map: map
+                })
+                busStop.infowindow = new google.maps.InfoWindow({
+                    content: busStop.indicator + " " + busStop.name
+                })
+            }
+            queryApi.nearStops.stopsList = data.stops
+            $state.go($state.current, {}, {
+                reload: true
+            });
+        })
+    }
+
+    queryApi.findBuses = function (busStop, map) {
+        var baseUrl = "https://transportapi.com/v3/uk/bus/stop/"
+        var url = baseUrl + busStop.atcocode + "/timetable.json?" + "app_id=" + app_id + "&app_key=" + app_key + "&callback=?"
+        var stopsList = queryApi.nearStops.stopsList
+
+        if (typeof (busStop.busesList) == "undefined") {
+            $.getJSON(url, function (data, status) {
+                console.log(url)
+                var busesList = []
+                Object.keys(data.departures).forEach(function (key, index) {
+                    var bus = data.departures[key][0]
+                    busesList.push(bus)
+                })
+                for (var i = 0; i < stopsList.length; i++) {
+                    if (busStop.atcocode != stopsList[i].atcocode) {} else {
+                        queryApi.findBuses.busesList = busesList
+                        stopsList[i].busesList = busesList
+                    }
+                }
+                queryApi.nearStops.stopsList = stopsList //triggers watcher in controller
+            })
+        }
+        console.log("queryApi findBuses busesList")
+    }
+}])
