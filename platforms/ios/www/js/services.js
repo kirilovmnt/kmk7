567f4f0a67f03b58055d45d5273c0d485abb9cc0
Kiril Kirilov <kirilov.mnt@gmail.com>
2016-08-03 19:22:36 +0100

    adding the accidentally ignored platforms and plugins

---
+++ platforms/ios/www/js/services.js
@@ -0,0 +1,103 @@
+angular.module('app.services', [])
+
+
+
+.service('initMap', [function () {
+
+    var initMapSrv = this
+    var mapOptions = {
+        center: {
+            lat: 54.5,
+            lng: -4
+        },
+        zoom: 5,
+        minZoom: 5
+    }
+    initMapSrv.initIn = function (element) {
+        initMapSrv.map = new google.maps.Map(element, mapOptions);
+    }
+    initMapSrv.marker = new google.maps.Marker({
+        title: "Your location",
+        icon: "img/pinpoint.gif",
+        optimized: false
+    })
+
+}])
+
+
+
+.service('geolocation', ['initMap', function (initMap) {
+    var geolocation = this
+
+
+    geolocation.setMapCenter = function () {
+        if (navigator.geolocation) {
+            this.watchID = navigator.geolocation.getCurrentPosition(geoSetSuccess, geoError, geoOptions)
+        } else {
+            //cordova geo plugin code goes here
+        }
+    }
+    geolocation.watchPosition = function () {
+        if (navigator.geolocation) {
+            this.watchID = navigator.geolocation.watchPosition(geoWatchSuccess, geoError, geoOptions)
+        } else {
+            //cordova geo plugin code goes here
+        }
+    }
+
+    var geoSetSuccess = function (position) {
+        var center = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        }
+
+        initMap.map.setCenter(center)
+        initMap.map.setZoom(13);
+        initMap.marker.setPosition(center)
+        initMap.marker.setMap(initMap.map);
+    }
+
+    var geoWatchSuccess = function (position) {
+        //set marker to current position
+        initMap.marker.setPosition({
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            })
+            //for debugging purposes
+            //console.log(position.timestamp + " <- Timestamp of response")
+    }
+
+    var geoError = function (error) {
+        switch (error.code) {
+            case error.PERMISSION_DENIED:
+                alert("User denied the request for Geolocation.")
+                break;
+            case error.POSITION_UNAVAILABLE:
+                alert("Location information is unavailable.")
+                break;
+            case error.TIMEOUT:
+                alert("The request to get user location timed out.")
+                break;
+            case error.UNKNOWN_ERROR:
+                alert("An unknown error occurred.")
+                break;
+        }
+    }
+
+    var geoOptions = {
+        enableHighAccuracy: true,
+        maximumAge: 30000,
+        timeout: 27000
+    }
+}])
+
+
+
+.service('')
+
+//
+//.factory('initMap', function () {
+//    return {map: function (element){
+//        var mapOptions = 
+//    }
+//})
